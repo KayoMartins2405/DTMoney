71fa67c057c103659bfcf239765529120ec6a00f
KayoMartins2405 <martinslima2424@gmail.com>
2024-05-05 22:43:43 -0300

    finalizando

--- js/modal.js
+++ js/modal.js
@@ -1,59 +1,53 @@
-// const inputdescricao = document.getElementById('input-descricao');
-// const inputpreco = document.getElementById('input-preco');
-// const inputcategoria = document.getElementById('input-categoria');
-// const btnAdd = document.getElementById('btnAdd');
-// const btnDelete = document.getElementById('btn-delete');
+const inputdescricao = document.getElementById('input-descricao');
+const inputpreco = document.getElementById('input-preco');
+const inputcategoria = document.getElementById('input-categoria');
+const btnAdd = document.getElementById('btnAdd');
+const btnDelete = document.getElementById('btn-delete');
 const container = document.querySelector('.container');
+const modal = document.querySelector('#myModal');
 const abrir = document.getElementById('openModalBtn');
+const fechar = document.getElementById('fechar');
 
-
-// inputpreco.addEventListener('change',(event)=> {
-//     if(event.target.value > 0){
-//         btnAdd.disabled = false;
-//     }else{
-//         btnAdd.disabled = true;
-//     }
+inputpreco.addEventListener('change',(event)=> {
+    if(event.target.value > 0){
+        btnAdd.disabled = false;
+    }else{
+        btnAdd.disabled = true;
+    }
    
-//     // console.log(event.target.value.length);
-// })
+    // console.log(event.target.value.length);
+})
 
 
 abrir.addEventListener("click", function () {
-    container.classList.remove('fechado');
-    abrir.classList.add('fechado');
-    console.log(abrir);
+    modal.classList.add('open');
+});
+fechar.addEventListener("click", function () {
+    modal.classList.remove('open');
 });
 
-// 
-// fechar.addEventListener("click", function(){
-//     container.classList.add('fechado');
-// })
+btnAdd.addEventListener("click", function (){
+    const descricao = inputdescricao.value;
+    const preco = inputpreco.value;
+    const categoria = inputcategoria.value;
+    const teste = `
+    <div class='itens'>  
+        <p>${descricao}</p>
+        <p>R$ ${preco}</p>
+        <p>${categoria}</p>
+        <button class='btn-delete' >x</button>
+    </div>
+    `;
+    lista.innerHTML += teste;
+    getElements();
+    console.log(btnAdd)
+});
 
-// fechar.addEventListener("click", function(){
-//     abrir.classList.remove('fechado');
-// })
-// btnAdd.addEventListener("click", function (){
-//     const descricao = inputdescricao.value;
-//     const preco = inputpreco.value;
-//     const categoria = inputcategoria.value;
-//     const teste = `
-//     <div class='itens'>  
-//         <p>${descricao}</p>
-//         <p>R$ ${preco}</p>
-//         <p>${categoria}</p>
-//         <button class='btn-delete' >x</button>
-//     </div>
-//     `;
-//     lista.innerHTML += teste;
-//     getElements();
-//     console.log(btnAdd)
-// });
-
-// function getElements() {
-//     const listaItems = document.querySelectorAll('.itens');
-//     listaItems.forEach(function (elemento) {
-//         elemento.addEventListener('click', function () {
-//             this.remove();
-//         })
-//     })
-// }
+function getElements() {
+    const listaItems = document.querySelectorAll('.itens');
+    listaItems.forEach(function (elemento) {
+        elemento.addEventListener('click', function () {
+            this.remove();
+        })
+    })
+}
